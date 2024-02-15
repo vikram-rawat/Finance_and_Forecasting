b815469436f8e36931599a4a1aca888d574e88c8
Anantadinath <vickey.coool@gmail.com>
2024-02-15 17:11:38 +0530

    update

---
+++ tidy_finance/chapter_04_tidy_fin.R
@@ -0,0 +1,14 @@
+# load libraries: ----------------------------------
+
+library(tidyverse)
+library(RSQLite)
+library(scales)
+library(slider)
+library(furrr)
+
+# Set defaults: ----------------------------------
+
+ggplot2::theme_set(theme_bw())
+
+# chapter 3: ----------------------------------
+
