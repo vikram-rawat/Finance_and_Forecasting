b81deaa2a24f389a150a22658d6f1a10dd572cf1
Anantadinath <vickey.coool@gmail.com>
2024-04-16 21:04:39 +0530

    completed chapter 15

--- torch/chap_15_img_class.R
+++ torch/chap_15_img_class.R
@@ -108,3 +108,103 @@ convnet <- nn_module(
 model <- convnet()
 img <- torch_randn(1, 1, 64, 64)
 model(img)
+
+# basic imagenet: ----------------------------------
+
+set.seed(777)
+torch_manual_seed(777)
+
+dir <- "torch-datasets"
+
+train_ds <- tiny_imagenet_dataset(
+  dir,
+  # download = TRUE,
+  transform = function(x) {
+    x |>
+      torchvision::transform_to_tensor()
+  }
+)
+
+valid_ds <- tiny_imagenet_dataset(
+  dir,
+  split = "val",
+  transform = function(x) {
+    x |>
+      torchvision::transform_to_tensor()
+  }
+)
+
+train_dl <- dataloader(train_ds,
+  batch_size = 128,
+  shuffle = TRUE
+)
+
+valid_dl <- dataloader(valid_ds, batch_size = 128)
+
+batch <- train_dl |>
+  dataloader_make_iter() |>
+  dataloader_next()
+
+dim(batch$x)
+dim(batch$y)
+
+
+convnet <- nn_module(
+  "convnet",
+  initialize = function() {
+    self$features <- nn_sequential(
+      nn_conv2d(3, 64, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_conv2d(64, 128, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_conv2d(128, 256, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_conv2d(256, 512, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_conv2d(512, 1024, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_adaptive_avg_pool2d(c(1, 1))
+    )
+    self$classifier <- nn_sequential(
+      nn_linear(1024, 1024),
+      nn_relu(),
+      nn_linear(1024, 1024),
+      nn_relu(),
+      nn_linear(1024, 200)
+    )
+  },
+  forward = function(x) {
+    x <- self$features(x)$squeeze()
+    x <- self$classifier(x)
+    x
+  }
+)
+
+fitted <- convnet |>
+  setup(
+    loss = nn_cross_entropy_loss(),
+    optimizer = optim_adam,
+    metrics = list(
+      luz_metric_accuracy()
+    )
+  ) |>
+  fit(
+    train_dl,
+    epochs = 50,
+    valid_data = valid_dl,
+    verbose = TRUE
+  )
+
+fitted |>
+  evaluate(data = valid_ds)
+
+preds <- fitted |>
+  predict(valid_ds)
+
+preds <- nnf_softmax(preds, dim = 2)
+
+torch_argmax(preds, dim = 2)
